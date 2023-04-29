85589d2bb3cd442c6138b3d39a9a2518fc232339
helloitsjoe <jboyle617@gmail.com>
2023-04-29 08:21:29 -0400

    params

--- main.go
+++ main.go
@@ -7,13 +7,33 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "OK",
-		})
+// query params
+// sub routes
+// headers
+// middleware/auth
+// cors
+// status codes
+// post body
+
+func home() {
+	// TODO: Serve HTML
+}
+
+func pong(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
 	})
+}
+
+func getUser(c *gin.Context) {
+	id := c.Param("id")
+	c.String(http.StatusOK, "Hello %s", id)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", pong)
+	router.GET("/user/:id", getUser)
 	fmt.Println("Listening on http://localhost:8080")
-	r.Run()
+	router.Run()
 }
